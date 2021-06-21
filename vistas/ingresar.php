597e88c70a45ab6e466bad649e3c13f2c9a5427b
Sebastian <dsebaortiz@gmail.com>
2021-06-21 15:09:12 -0300

    Style form

--- vistas/ingresar.php
+++ vistas/ingresar.php
@@ -4,16 +4,23 @@ include "parciales/header.php";
  ?>
 <section class="fdb-block bg-dark bg-image-over">
 	<div class="container">
-		   <form id="Iniciar" method="post">
-		 	  <div class="form">
-			  <h4>Iniciar Sesión</h4>
-		        <input type="hidden" name="action" value="loguear_usuario">
-				<input type="text" id="user" placeholder="&#128273;user" name="usuario">
-				<input type="password" id="password" placeholder="&#128273;ingresa tu contraseña" name="password">
-				<input type="submit" value="Iniciar sesión">		
-	         </div>
-           </form>
-
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <form id="Iniciar" method="post">
+                    <div class="form">
+                    <h3 class="mb-4">Iniciar Sesión</h3>
+                    <input type="hidden" name="action" value="loguear_usuario">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="user" placeholder="&#128273; Usuario" name="usuario">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="password" placeholder="&#128273; Ingresá tu contraseña" name="password">
+                    </div>
+                    <input type="submit" class="btn btn-warning" value="Iniciar sesión">		
+                    </div>
+                </form>
+            </div>
+        </div>
 	</div>
 </section>
 
